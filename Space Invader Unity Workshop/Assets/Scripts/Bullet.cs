b8c8d4b9571ec9659eb82f41d6964a62d83603d1
SayanBanerjee2000 <119033005+SayanBanerjee2000@users.noreply.github.com>
2023-01-15 16:14:56 +0530

    Add files via upload

---
+++ Space Invader Unity Workshop/Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = speed * Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Enemy bullet hits player
+        if (this.tag == "Bullet-E" && collision.tag == "Player")
+            Destroy(gameObject);
+
+        //Player bullet hits a target
+
+        if (this.tag == "Bullet-P")
+            Destroy(gameObject);
+
+        //Bullet (either enemy or player) hits the barrier
+        //Destroy the bullet as well the barrier
+        if(collision.tag == "Barrier")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Space Invader Unity Workshop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Text lives;
+    public Text score;
+    public Text result;
+
+    public static int enemiesKilled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesKilled = 0;
+        lives.text = "LIVES: 3";
+        score.text = "SCORE: 0";
+        result.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives.text = "LIVES: " + Player.lives.ToString();
+        score.text = "SCORE: " + (enemiesKilled * 10).ToString();
+
+        if (Player.lives == 0)
+            result.text = "GAME OVER";
+        else if (enemiesKilled == 28)
+            result.text = "YOU WON!";
+
+        //Restart, Exit
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(0);
+        else if (Input.GetKeyDown(KeyCode.E))
+            Application.Quit();
+
+    }
+}
